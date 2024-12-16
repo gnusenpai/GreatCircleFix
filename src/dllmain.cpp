a4e9e04bf6b2ef65b2f81f5f462f199479d4cb74
Lyall <lyall@stamper.net>
2024-12-16 23:44:46 +0000

    Add toggles for each feature.

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -3,6 +3,7 @@
 
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
+#include <inipp/inipp.h>
 #include <safetyhook.hpp>
 
 #define spdlog_confparse(var) spdlog::info("Config Parse: {}: {}", #var, var)
@@ -14,6 +15,10 @@ HMODULE thisModule;
 std::string sFixName = "GreatCircleFix";
 std::string sFixVersion = "0.0.3";
 std::filesystem::path sFixPath;
+
+// Ini
+inipp::Ini<char> ini;
+std::string sConfigFile = sFixName + ".ini";
 
 // Logger
 std::shared_ptr<spdlog::logger> logger;
@@ -31,6 +36,12 @@ float fHUDWidth;
 float fHUDWidthOffset;
 float fHUDHeight;
 float fHUDHeightOffset;
+
+// Ini variables
+bool bSkipIntro;
+bool bFixCutsceneFOV;
+bool bFixCulling;
+bool bUnrestrictCVars;
 
 // Variables
 int iCurrentResX;
@@ -80,6 +91,44 @@ void Logging()
     }  
 }
 
+void Configuration()
+{
+    // Inipp initialisation
+    std::ifstream iniFile(sFixPath.string() + sConfigFile);
+    if (!iniFile) {
+        AllocConsole();
+        FILE* dummy;
+        freopen_s(&dummy, "CONOUT$", "w", stdout);
+        std::cout << "" << sFixName.c_str() << " v" << sFixVersion.c_str() << " loaded." << std::endl;
+        std::cout << "ERROR: Could not locate config file." << std::endl;
+        std::cout << "ERROR: Make sure " << sConfigFile.c_str() << " is located in " << sFixPath.string().c_str() << std::endl;
+        spdlog::shutdown();
+        FreeLibraryAndExitThread(thisModule, 1);
+    }
+    else {
+        spdlog::info("Config file: {}", sFixPath.string() + sConfigFile);
+        ini.parse(iniFile);
+    }
+
+    // Parse config
+    ini.strip_trailing_comments();
+    spdlog::info("----------");
+
+    // Load settings from ini
+    inipp::get_value(ini.sections["Skip Intro"], "Enabled", bSkipIntro);
+    inipp::get_value(ini.sections["Unrestrict CVars"], "Enabled", bUnrestrictCVars);
+    inipp::get_value(ini.sections["Fix Cutscene FOV"], "Enabled", bFixCutsceneFOV);
+    inipp::get_value(ini.sections["Fix Culling"], "Enabled", bFixCulling);
+
+    // Log ini parse
+    spdlog_confparse(bSkipIntro);
+    spdlog_confparse(bUnrestrictCVars);
+    spdlog_confparse(bFixCutsceneFOV);
+    spdlog_confparse(bFixCulling);
+
+    spdlog::info("----------");
+}
+
 void CalculateAspectRatio(bool bLog)
 {
     if (iCurrentResX <= 0 || iCurrentResY <= 0)
@@ -123,21 +172,23 @@ void CalculateAspectRatio(bool bLog)
 
 void SkipIntro()
 {
-    // com_skipIntroVideo
-    std::uint8_t* SkipIntroVideoScanResult = Memory::PatternScan(exeModule, "0F 95 ?? ?? ?? FF 15 ?? ?? ?? ?? 48 8B ?? ?? 48 8D ?? ?? ?? ?? ??");
-    if (SkipIntroVideoScanResult) {
-        spdlog::info("Skip Intro Video: Address is {:s}+{:x}", sExeName.c_str(), SkipIntroVideoScanResult - (std::uint8_t*)exeModule);
-        static SafetyHookMid SkipIntroVideoMidHook{};
-        SkipIntroVideoMidHook = safetyhook::create_mid(SkipIntroVideoScanResult,
-            [](SafetyHookContext& ctx) {
-                // Clear ZF
-                ctx.rflags &= ~(1 << 6);
+    if (bSkipIntro) {
+        // com_skipIntroVideo
+        std::uint8_t* SkipIntroVideoScanResult = Memory::PatternScan(exeModule, "0F 95 ?? ?? ?? FF 15 ?? ?? ?? ?? 48 8B ?? ?? 48 8D ?? ?? ?? ?? ??");
+        if (SkipIntroVideoScanResult) {
+            spdlog::info("Skip Intro Video: Address is {:s}+{:x}", sExeName.c_str(), SkipIntroVideoScanResult - (std::uint8_t*)exeModule);
+            static SafetyHookMid SkipIntroVideoMidHook{};
+            SkipIntroVideoMidHook = safetyhook::create_mid(SkipIntroVideoScanResult,
+                [](SafetyHookContext& ctx) {
+                    // Clear ZF
+                    ctx.rflags &= ~(1 << 6);
 
-                spdlog::info("Skip Intro Video: Skipped intro videos.");
-            });
-    }
-    else {
-        spdlog::error("Skip Intro Video: Pattern scan failed.");
+                    spdlog::info("Skip Intro Video: Skipped intro videos.");
+                });
+        }
+        else {
+            spdlog::error("Skip Intro Video: Pattern scan failed.");
+        }
     }
 }
 
@@ -172,30 +223,32 @@ void SetCVar(const std::string& cvarString)
 
 void CVars()
 {
-    // Remove cvar restrictions
-    std::uint8_t* CVarRestrictionsScanResult = Memory::PatternScan(exeModule, "BA 01 00 00 00 49 ?? ?? 44 ?? ?? 41 FF ?? ?? 66 0F ?? ?? ?? ?? ?? ??");
-    if (CVarRestrictionsScanResult) {
-        spdlog::info("CVar Restrictions: Address is {:s}+{:x}", sExeName.c_str(), CVarRestrictionsScanResult - (std::uint8_t*)exeModule);
-        Memory::Write(CVarRestrictionsScanResult + 0x1, (int)0);
-        spdlog::info("CVar Restrictions: Disabled cvar restrictions.");
-    }
-    else {
-        spdlog::error("CVar Restrictions: Pattern scan failed.");
-    }
+    if (bUnrestrictCVars) {
+        // Remove cvar restrictions
+        std::uint8_t* CVarRestrictionsScanResult = Memory::PatternScan(exeModule, "BA 01 00 00 00 49 ?? ?? 44 ?? ?? 41 FF ?? ?? 66 0F ?? ?? ?? ?? ?? ??");
+        if (CVarRestrictionsScanResult) {
+            spdlog::info("CVar Restrictions: Address is {:s}+{:x}", sExeName.c_str(), CVarRestrictionsScanResult - (std::uint8_t*)exeModule);
+            Memory::Write(CVarRestrictionsScanResult + 0x1, (int)0);
+            spdlog::info("CVar Restrictions: Disabled cvar restrictions.");
+        }
+        else {
+            spdlog::error("CVar Restrictions: Pattern scan failed.");
+        }
 
-    // Remove read-only flag check for cvars
-    std::uint8_t* ReadOnlyCvarScanResult = Memory::PatternScan(exeModule, "0F ?? ?? 0E 73 ?? 48 8B ?? ?? 48 8D ?? ?? ?? ?? ?? E8 ?? ?? ?? ??");
-    if (ReadOnlyCvarScanResult) {
-        spdlog::info("Read-Only Cvars: Address is {:s}+{:x}", sExeName.c_str(), ReadOnlyCvarScanResult - (std::uint8_t*)exeModule);
-        static SafetyHookMid ReadOnlyCvarMidHook{};
-        ReadOnlyCvarMidHook = safetyhook::create_mid(ReadOnlyCvarScanResult,
-            [](SafetyHookContext& ctx) {
-                // Clear read-only flag (bit 15)
-                ctx.rax &= ~(1 << 15);
-            });
-    }
-    else {
-        spdlog::error("Read-Only Cvars: Pattern scan failed.");
+        // Remove read-only flag check for cvars
+        std::uint8_t* ReadOnlyCvarScanResult = Memory::PatternScan(exeModule, "0F ?? ?? 0E 73 ?? 48 8B ?? ?? 48 8D ?? ?? ?? ?? ?? E8 ?? ?? ?? ??");
+        if (ReadOnlyCvarScanResult) {
+            spdlog::info("Read-Only Cvars: Address is {:s}+{:x}", sExeName.c_str(), ReadOnlyCvarScanResult - (std::uint8_t*)exeModule);
+            static SafetyHookMid ReadOnlyCvarMidHook{};
+            ReadOnlyCvarMidHook = safetyhook::create_mid(ReadOnlyCvarScanResult,
+                [](SafetyHookContext& ctx) {
+                    // Clear read-only flag (bit 15)
+                    ctx.rax &= ~(1 << 15);
+                });
+        }
+        else {
+            spdlog::error("Read-Only Cvars: Pattern scan failed.");
+        }
     }
 
     // Get idCmdSystemLocal
@@ -233,9 +286,11 @@ void CVars()
         static SafetyHookMid LevelLoadCompletedMidHook{};
         LevelLoadCompletedMidHook = safetyhook::create_mid(LevelLoadCompletedScanResult,
             [](SafetyHookContext& ctx) {
-                // Fix culling issues
-                SetCVar("r_gpuTriangleCullingOptions 0");
-                SetCVar("r_gpugatherskipocclusionpass 1");
+                if (bFixCulling) {
+                    // Fix culling issues
+                    SetCVar("r_gpuTriangleCullingOptions 0");
+                    SetCVar("r_gpugatherskipocclusionpass 1");
+                }
             });
     }
     else {
@@ -251,48 +306,51 @@ void AspectRatioFOV()
     iCurrentResY = DesktopDimensions.second;
     CalculateAspectRatio(true);
 
-    // Cutscene FOV
-    std::uint8_t* CutsceneFOVScanResult = Memory::PatternScan(exeModule, "83 ?? ?? ?? 02 0F 28 ?? 48 8B ?? ?? ?? 0F 57 ?? F3 0F ?? ?? F3 0F ?? ?? ?? ?? ?? ??");
-    if (CutsceneFOVScanResult) {
-        spdlog::info("Cutscene FOV: Address is {:s}+{:x}", sExeName.c_str(), CutsceneFOVScanResult - (std::uint8_t*)exeModule);
-        static SafetyHookMid CutsceneFOVMidHook{};
-        CutsceneFOVMidHook = safetyhook::create_mid(CutsceneFOVScanResult + 0x8,
-            [](SafetyHookContext& ctx) {
-                // Check for "Fullscreen" picture framing option
-                if (ctx.rax == 0) {
-                    // Get current resolution
-                    int iResX = (int)ctx.xmm1.f32[0];
-                    int iResY = (int)ctx.xmm2.f32[0];
+    if (bFixCutsceneFOV) {
+        // Cutscene FOV
+        std::uint8_t* CutsceneFOVScanResult = Memory::PatternScan(exeModule, "83 ?? ?? ?? 02 0F 28 ?? 48 8B ?? ?? ?? 0F 57 ?? F3 0F ?? ?? F3 0F ?? ?? ?? ?? ?? ??");
+        if (CutsceneFOVScanResult) {
+            spdlog::info("Cutscene FOV: Address is {:s}+{:x}", sExeName.c_str(), CutsceneFOVScanResult - (std::uint8_t*)exeModule);
+            static SafetyHookMid CutsceneFOVMidHook{};
+            CutsceneFOVMidHook = safetyhook::create_mid(CutsceneFOVScanResult + 0x8,
+                [](SafetyHookContext& ctx) {
+                    // Check for "Fullscreen" picture framing option
+                    if (ctx.rax == 0) {
+                        // Get current resolution
+                        int iResX = (int)ctx.xmm1.f32[0];
+                        int iResY = (int)ctx.xmm2.f32[0];
 
-                    if (iResX != iCurrentResX || iResY != iCurrentResY) {
-                        iCurrentResX = iResX;
-                        iCurrentResY = iResY;
-                        CalculateAspectRatio(true);
+                        if (iResX != iCurrentResX || iResY != iCurrentResY) {
+                            iCurrentResX = iResX;
+                            iCurrentResY = iResY;
+                            CalculateAspectRatio(true);
+                        }
+
+                        if (fAspectRatio > fNativeAspect) {
+                            // Set ZF to jump over 16:9 FOV modification
+                            ctx.rflags |= (1ULL << 6);
+
+                            // Fix vert- FOV
+                            ctx.xmm3.f32[0] = atanf(tanf(ctx.xmm3.f32[0] * (fPi / 360)) / fNativeAspect * fAspectRatio) * (360 / fPi);
+                        }
                     }
-
-                    if (fAspectRatio > fNativeAspect) {
-                        // Set ZF to jump over 16:9 FOV modification
-                        ctx.rflags |= (1ULL << 6);
-
-                        // Fix vert- FOV
-                        ctx.xmm3.f32[0] = atanf(tanf(ctx.xmm3.f32[0] * (fPi / 360)) / fNativeAspect * fAspectRatio) * (360 / fPi);
-                    }
-                }
-            });
+                });
+        }
+        else {
+            spdlog::error("Cutscene FOV: Pattern scan failed.");
+        }
     }
-    else {
-        spdlog::error("Cutscene FOV: Pattern scan failed.");
-    } 
 }
 
 void Framerate()
 {
-    
+    // TODO
 }
 
 DWORD __stdcall Main(void*)
 {
     Logging();
+    Configuration();
     SkipIntro();
     CVars();
     AspectRatioFOV();
