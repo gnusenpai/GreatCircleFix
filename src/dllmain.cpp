c0462af79eb628d72a99607aae852c8d70d9a012
Lyall <lyall@stamper.net>
2025-02-21 13:28:12 +0000

    Update dllmain.cpp

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -13,7 +13,7 @@ HMODULE thisModule;
 
 // Fix details
 std::string sFixName = "GreatCircleFix";
-std::string sFixVersion = "0.0.5";
+std::string sFixVersion = "0.0.6";
 std::filesystem::path sFixPath;
 
 // Ini
